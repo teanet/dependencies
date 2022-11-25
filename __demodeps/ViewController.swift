2e20a7bb39fbe5c3aac56f571d3beb66a18a459f
e.tyutyuev <e.tyutyuev@2gis.ru>
2022-11-25 12:27:04 +0700

    Initial Commit

---
+++ __demodeps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  __demodeps
+//
+//  Created by teanet on 25.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
